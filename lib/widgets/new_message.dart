7d685e2fdc7169395a97084ebe36b4472caafec0
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-08-17 12:31:30 +0330

    Add message input in chat screen

---
+++ lib/widgets/new_message.dart
@@ -0,0 +1,63 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class NewMessages extends StatefulWidget {
+  const NewMessages({super.key});
+
+  @override
+  State<NewMessages> createState() {
+    return _NewMessagesState();
+  }
+}
+
+class _NewMessagesState extends State<NewMessages> {
+  var _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void _submitMessage() {
+    final message = _messageController.text;
+
+    if (message.trim().isEmpty) {
+      return;
+    }
+
+    _messageController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 15,
+        right: 1,
+        bottom: 14,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _messageController,
+              textCapitalization: TextCapitalization.sentences,
+              autocorrect: true,
+              enableSuggestions: true,
+              decoration: InputDecoration(labelText: 'Send a message ...'),
+            ),
+          ),
+          IconButton(
+            onPressed: _submitMessage,
+            icon: Icon(
+              Icons.send,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
